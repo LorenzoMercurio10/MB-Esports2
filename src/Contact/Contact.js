c2a0910ca0c0c1d519de168d8bb84a72c5754f71
LorenzoMercurio10 <lorenzo.mercurio@studenti.polito.it>
2022-09-20 18:44:06 +0200

    start working on 'Contact us'

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,4 +1,6 @@
 import './Contact.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Contact() {
     return (
@@ -15,8 +17,27 @@ function Contact() {
                 </div>
             </div>
         </section>
+    <Form className="form-contact">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+                Scrivi qui la mail al quale vorrai essere ricontattato
+            </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formBasicPassword">
+            <Form.Label>Messaggio:</Form.Label>
+                <Form.Control type="password" placeholder="Componi il tuo messaggio" />
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button className="button-contact" variant="primary" type="submit">
+            Submit
+        </Button>
+    </Form>
     </>
-    )
+ )
 }
 
 export default Contact;
