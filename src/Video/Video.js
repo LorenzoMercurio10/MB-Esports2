5b7b3238d5578e4d8c1b24a4baaa2b7aca41a0d4
LorenzoMercurio10 <lorenzo.mercurio@studenti.polito.it>
2022-10-12 12:27:41 +0200

    video changed

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -5,7 +5,7 @@ function Video() {
         <section id="home">
             <div>
                 <div className="video ratio ratio-16x9">
-                    <iframe src="https://www.youtube.com/embed/Juq39r6HRmI?&autoplay=1&mute=1&controls=0&loop=1" title="YouTube video" allowFullScreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/e-O-cDxHJ-0?&autoplay=1&mute=1&controls=0&loop=1" title="YouTube video" allowFullScreen></iframe>
                 </div>
             </div>
         </section>
